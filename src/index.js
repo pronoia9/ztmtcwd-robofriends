61d54315225c3b145109a0da2889815ef5078fbf
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 09:56:58 +0300

    map robots data and render all the cards

--- src/index.js
+++ src/index.js
@@ -10,6 +10,6 @@ import robots from './robots.json';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
+    {robots.map(robot => <Card id={robot.id} name={robot.name} username={robot.username} email={robot.email} />)}
   </React.StrictMode>
 );
