5cc917682980aa2f39907babde9130571a2e1b18
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 11:43:57 +0300

    add default props

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Card = ({ id, name, username, email }) => {
+const Card = ({ id, name = 'Name', username = 'Username', email = 'example@mail.com' }) => {
   return (
     <div className='card tc bg-light-green dib bd3 pa3 ma2 grow bw2 shadow-5'>
       <img src={`https://robohash.org/${id}?200x200`} alt={`${username}_photo`} />
