982f14cbdde01d39da53a912cf38dc7c4405448b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:14:08 +0300

    change app to class

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
 // data
@@ -6,15 +6,18 @@ import robots from './robots.json';
 
 const state = {
   robots: robots,
-  searchKey: ''
-}
-const App = ({ robots }) => {
-  return (
-    <div className='app-container'>
-      <SearchBox searchKey={state.searchKey} />
-      {state.robots.length > 0 ? <CardList robots={state.robots} searchKey={state.searchKey} /> : <></>}
-    </div>
-  );
+  searchKey: '',
 };
+
+class App extends Component {
+  render() {
+    return (
+      <div className='app-container'>
+        <SearchBox searchKey={state.searchKey} />
+        {state.robots.length > 0 ? <CardList robots={state.robots} searchKey={state.searchKey} /> : <></>}
+      </div>
+    );
+  }
+}
 
 export default App;
