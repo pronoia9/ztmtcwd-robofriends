736a51d2f72b2bed7bb60de638a42484fd764ff6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 11:38:57 +0300

    add check for robots array

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import CardList from './CardList';
 
 const App = ({ robots }) => {
-  return <div className='app-container'>{robots ? <CardList robots={robots} /> : <></>}</div>;
+  return <div className='app-container'>{robots.length > 0 ? <CardList robots={robots} /> : <></>}</div>;
 };
 
 export default App;
