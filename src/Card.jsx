10f5a025901d19285a5e9b79332893b593334162
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 15:15:06 +0300

    change card div layout

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,11 +3,13 @@ import './css/Card.css';
 
 const Card = ({ id, name = 'Name', username = 'Username', email = 'example@mail.com' }) => {
   return (
-    <div className='card tc dib bd3 pa3 ma2 grow bw2'>
-      <img src={`https://robohash.org/${id}?200x200`} alt={`${username}_photo`} />
-      <div>
-        <h2>{name}</h2>
-        <p>{email}</p>
+    <div className='card-container tc dib bd3 pa3 ma2 grow bw2'>
+      <div className='card'>
+        <img src={`https://robohash.org/${id}?200x200`} alt={`${username}_photo`} />
+        <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+        </div>
       </div>
     </div>
   );
