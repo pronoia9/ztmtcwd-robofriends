bbf7cb72da06f306bc70ac3a1a8b2b636ff2370d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 07:24:42 +0300

    add alt attribute

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
   return (
     <div>
-      <img src='' />
+      <img src='' alt='photo' />
       <div>
         <h2>Name</h2>
         <p>email@gmail.com</p>
