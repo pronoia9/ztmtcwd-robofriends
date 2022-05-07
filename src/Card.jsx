76e2c8ee3ad17468772c5d8689d307883d761268
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 08:12:56 +0300

    add tachyon css to card

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div>
+    <div className='bg-light-green dib bd3 pa3 ma2 grow bw2 shadow-5'>
       <img src='https://robohash.org/test?200x200' alt='photo' />
       <div>
         <h2>Name</h2>
