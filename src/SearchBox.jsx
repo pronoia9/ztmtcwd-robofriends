2c488e3c317c7973167899a0bb52edafcf70deec
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 14:41:05 +0300

    remove tachyons css classes

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,7 +4,7 @@ import './css/SearchBox.css';
 const SearchBox = ({ onSearch }) => {
   return (
     <div className='searchbox-container pa2 tc'>
-      <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={onSearch} />
+      <input className='' type='search' placeholder='search robots' onChange={onSearch} />
     </div>
   );
 };
