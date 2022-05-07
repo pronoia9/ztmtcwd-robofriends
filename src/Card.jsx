fd3874e019e3b232da9629a85d714a9c0de6df9f
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 07:23:52 +0300

    add simple card content

---
+++ src/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img src='' />
+      <div>
+        <h2>Name</h2>
+        <p>email@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
