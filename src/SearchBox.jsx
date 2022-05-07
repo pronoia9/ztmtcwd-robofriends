b4040c7cea04106d3dd38646bba7d32e358a96ba
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:30:09 +0300

    remove searchkey prop

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const SearchBox = ({ onSearch, searchKey }) => {
+const SearchBox = ({ onSearch }) => {
   return (
     <div className='searchbox-container pa2 tc'>
       <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={onSearch} />
