5b57c25cb0741434d6d9278a0b100c9662f73955
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 11:48:46 +0300

    import searchbox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import SearchBox from './SearchBox';
 import CardList from './CardList';
 
 const App = ({ robots }) => {
-  return <div className='app-container'>{robots.length > 0 ? <CardList robots={robots} /> : <></>}</div>;
+  return (
+    <div className='app-container'>
+      <SearchBox />
+      {robots.length > 0 ? <CardList robots={robots} /> : <></>}
+    </div>
+  );
 };
 
 export default App;
