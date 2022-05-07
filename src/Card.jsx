96d44a5a244a8c92ee5708436397b2523954f8fd
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 07:26:35 +0300

    add robohash size

--- src/Card.jsx
+++ src/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
   return (
     <div>
-      <img src='https://robohash.org/test' alt='photo' />
+      <img src='https://robohash.org/test?200x200' alt='photo' />
       <div>
         <h2>Name</h2>
         <p>email@gmail.com</p>
