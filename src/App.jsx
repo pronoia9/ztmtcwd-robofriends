71fd1144c219465b920178f78baaa9fac4b0d952
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:44:30 +0300

    add filter to robots array sent to card list

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,15 @@ class App extends Component {
     };
   }
 
-  onSearch = (e) => { this.state.searchKey = e.target.value; };
+  onSearch = (e) => {
+    console.log(`key: ${this.state.searchKey}  |  input: ${e.target.value}`);
+  };
 
   render() {
     return (
       <div className='app-container'>
         <SearchBox onSearch={this.onSearch} />
-        <CardList robots={this.state.robots} searchKey={this.state.searchKey} />
+        <CardList robots={this.state.robots.filter((robot) => robot.name.toLowerCase().includes(this.state.searchKey.toLowerCase()))} searchKey={this.state.searchKey} />
       </div>
     );
   }
