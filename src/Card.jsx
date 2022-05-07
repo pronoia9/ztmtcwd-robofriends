f2df7925724348bfae655d49f8cf34e919c122b7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 11:19:47 +0300

    add card class

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Card = ({id, name, username, email}) => {
+const Card = ({ id, name, username, email }) => {
   return (
-    <div className='tc bg-light-green dib bd3 pa3 ma2 grow bw2 shadow-5'>
+    <div className='card tc bg-light-green dib bd3 pa3 ma2 grow bw2 shadow-5'>
       <img src={`https://robohash.org/${id}?200x200`} alt={`${username}_photo`} />
       <div>
         <h2>{name}</h2>
