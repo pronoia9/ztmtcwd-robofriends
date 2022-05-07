513d1722e803fe58e210340f8148ccf9e4ad43a3
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:25:56 +0300

    change robots map + check into filter + map

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+const onSearch = (e) => {
+  console.log(e.target.value);
+};
+
 const SearchBox = ({ searchKey }) => {
   return (
     <div className='searchbox-container pa2 tc'>
-      <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={e => searchKey = e.target.value} />
+      <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={onSearch} />
     </div>
   );
 };
