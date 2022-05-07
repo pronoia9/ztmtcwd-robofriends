da26df81d2adc19aa3d3055bdb3f944e4f9b41e4
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:11:01 +0300

    add searchkey prop

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({ searchKey }) => {
   return (
     <div className='searchbox-container pa2 tc'>
-      <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={e => console.log(e.target.value)} />
+      <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={e => searchKey = e.target.value} />
     </div>
   );
 };
