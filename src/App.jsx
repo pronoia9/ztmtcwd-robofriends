5f217243bf9570181508ab6b8de4bf64bb598b58
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:30:17 +0300

    add onsearch function

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import CardList from './CardList';
 // data
 import robots from './robots.json';
 
+const onSearch = (e) => {
+  // this.state.searchKey = e.target.value;
+  console.log(`${this.state.searchKey}   |   ${e.target.value}`);
+};
 class App extends Component {
   constructor() {
     super();
@@ -15,7 +19,7 @@ class App extends Component {
   render() {
     return (
       <div className='app-container'>
-        <SearchBox searchKey={this.state.searchKey} />
+        <SearchBox onSearch={onSearch} />
         <CardList robots={this.state.robots} searchKey={this.state.searchKey} />
       </div>
     );
