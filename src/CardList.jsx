a8a9fe24a1538d9a98c6c7efefe3dbec1d0bd535
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:44:54 +0300

    remove filtter

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -4,8 +4,7 @@ import Card from './Card';
 const Cards = ({ robots, searchKey }) => {
   return (
     <div className='cards-container'>
-      {robots.filter(robot => robot.name.toLowerCase(searchKey.toLowerCase()))
-             .map(robot => <Card {...robot} key={robot.id} />)}
+      {robots.map(robot => <Card {...robot} key={robot.id} />)}
     </div>
   );
 };
