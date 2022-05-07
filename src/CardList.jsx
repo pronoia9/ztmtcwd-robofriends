6c9024e654f63092f77f5135b8eede14f99318eb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 13:50:43 +0300

    center container

--- src/CardList.jsx
+++ src/CardList.jsx
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const Cards = ({ robots, searchKey }) => {
   return (
-    <div className='cards-container'>
+    <div className='cards-container tc'>
       {robots.map(robot => <Card {...robot} key={robot.id} />)}
     </div>
   );
