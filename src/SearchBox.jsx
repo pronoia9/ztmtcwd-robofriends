8639810d426da0a170a118cce3bacbaa8852f476
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:29:09 +0300

    add onsearch function to props

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-const onSearch = (e) => {
-  const key = this.state.searchKey;
-};
-
-const SearchBox = ({ searchKey }) => {
+const SearchBox = ({ onSearch, searchKey }) => {
   return (
     <div className='searchbox-container pa2 tc'>
       <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={onSearch} />
