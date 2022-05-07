826f6b2464870f5068025c703915f34017b45de8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 09:52:18 +0300

    add props from robots data

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='bg-light-green dib bd3 pa3 ma2 grow bw2 shadow-5'>
-      <img src='https://robohash.org/test?200x200' alt='photo' />
+      <img src={`https://robohash.org/${props.id}?200x200`} alt={`photo${props.id}`} />
       <div>
-        <h2>Name</h2>
-        <p>email@gmail.com</p>
+        <h2>{props.name}</h2>
+        <p>{props.email}</p>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// components
 import Card from './Card';
+// styles
 import './index.css';
+// data
+import robots from './robots.json';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Card />
+    <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email} />
   </React.StrictMode>
 );
