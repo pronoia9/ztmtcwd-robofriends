06a6834b32bd87e0262ba6edc345e311558f4a2e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 11:51:52 +0300

    add css props

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const SearchBox = () => {
-  return <div className='searchbox-container'>
-    <input type='search' placeholder='search robots' />
-  </div>;
+  return (
+    <div className='searchbox-container pa2 tc'>
+      <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' />
+    </div>
+  );
 };
 
 export default SearchBox;
