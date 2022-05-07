cf4a8edec29498e43acc7c7f9a3eef286cb73a50
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-07 12:03:03 +0300

    add onchange to get search input

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SearchBox = () => {
   return (
     <div className='searchbox-container pa2 tc'>
-      <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' />
+      <input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='search robots' onChange={e => console.log(e.target.value)} />
     </div>
   );
 };
