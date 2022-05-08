417957bbf3830fbe624700e6678b752fdb4aa3ef
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-08 07:14:06 +0300

    organize lifecycle

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import SearchBox from './SearchBox';
 import CardList from './CardList';
 // data
-import robotsData from './robots.json';
+import robotsData from './robots.json';  //https://jsonplaceholder.typicode.com/users
 // styles
 import './css/App.css';
 
 class App extends Component {
+  // MOUNTING FUNCTIONS
   constructor() {
     super();
     this.state = {
@@ -15,13 +16,18 @@ class App extends Component {
     };
   }
 
-  onSearch = (e) => {
-    this.setState({ ...this.state, searchKey: e.target.value });
-
-    e.target.value === '' ? (document.getElementsByTagName('html')[0].style.height = '') : (document.getElementsByTagName('html')[0].style.height = '100%');
-  };
+  componentWillMount() {}
 
   render() {
+    // fetch('https://jsonplaceholder.typicode.com/users')
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     // robotsData.concat(data);
+    //     data.map((e) => this.state.robots.push(e));
+    //     console.log(`fetch: ${this.state.robots}`);
+    //   });
+    // console.log(`render: ${this.state.robots}`);
+
     return (
       <div className='test-container'>
         <div className='app-container'>
@@ -29,11 +35,28 @@ class App extends Component {
             <h1 className='title'>Robofriends</h1>
           </div>
           <SearchBox onSearch={this.onSearch} />
-          <CardList robots={this.state.robots.filter((robot) => robot.name.toLowerCase().includes(this.state.searchKey.toLowerCase()))} searchKey={this.state.searchKey} />
+          <CardList robots={this.state.robots ? this.state.robots.filter((robot) => robot.name.toLowerCase().includes(this.state.searchKey.toLowerCase())) : ''} searchKey={this.state.searchKey} />
         </div>
       </div>
     );
   }
+
+  componentDidMount() {}
+
+  // UPDATING FUNCTIONS
+  componentWillReceiveProps() {}
+  shouldComponentUpdate() {}
+  componentWillUpdate() {}
+  componentDidUpdate() {}
+
+  // UNMOUNTING FUNCTIONS
+
+  // OTHER FUNCTIONS
+  onSearch = (e) => {
+    this.setState({ ...this.state, searchKey: e.target.value });
+
+    e.target.value === '' ? (document.getElementsByTagName('html')[0].style.height = '') : (document.getElementsByTagName('html')[0].style.height = '100%');
+  };
 }
 
 export default App;
