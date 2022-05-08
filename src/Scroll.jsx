d4bdbf678dac319e4b7676c60512d6adc22fd3fb
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-08 10:55:32 +0300

    import scroll styles

--- src/Scroll.jsx
+++ src/Scroll.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import './css/Scroll.css';
 
 const Scroll = (props) => {
-  return <div className='scroll-container'>{ props.children }</div>;
+  return <div className='scroll-container'>{props.children}</div>;
 };
 
 export default Scroll;
