54b4bff4c1b003aacb7050d34b85043d7017597b
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-05-08 16:56:14 +0300

    add error boundry component

---
+++ src/components/ErrorBoundry.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+  constructor() {
+    super();
+    this.state = {
+      hasError: false,
+      error: '',
+      info: '',
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true, error: error, info: info });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Ooooops. That's not good.</h1>;
+    }
+  }
+}
+
+export default ErrorBoundry;
