039b949856fd6b2f94faa3715592c074f55e9c7e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-05 17:43:04 +0300

    fix class name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import Div from '../utils/Div';
 export default function Header() {
   return (
     <Div classNames={['row', 'col-lg-12', 'section-title text-center']}>
-      <span classNames='subtitle'>A friendly search app for your Robofriends</span>
-      <h2 classNames='title'>Robofriends</h2>
+      <span className='subtitle'>A friendly search app for your Robofriends</span>
+      <h2 className='title'>Robofriends</h2>
     </Div>
   );
 }
