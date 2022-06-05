b11ffaaa993d3fa5f59d1193c92e8df2f5e6cdb6
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-05 15:54:01 +0300

    change func name

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './SearchBox.css';
 
-const SearchBox = ({ onSearch }) => {
+const SearchBox = ({ onInputChange }) => {
   return (
     <div className='searchbox-container pa2 tc'>
-      <input className='search-box' type='search' placeholder='search robots' onChange={onSearch} />
+      <input className='search-box' type='search' placeholder='search robots' onChange={onInputChange} />
     </div>
   );
 };
