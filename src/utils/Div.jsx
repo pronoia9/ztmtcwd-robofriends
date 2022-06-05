b8822a38833572dfde9024526f4dfa087a7543b5
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-05 16:01:52 +0300

    add div component

---
+++ src/utils/Div.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Div({ children, properties = [], classNames = [], ids = [], index = 0 }) {
+  // console.log('properties[', index, ']:', properties[index])
+  // console.log(Object.keys(properties[index]), '=', Object.values(properties[index]));
+
+  return (
+    // <div className={classNames[index]} id={ids[index]}>
+    //   {(index < properties.length - 1) ? <Div properties={properties} classNames={classNames} ids={ids} index={index + 1}>{children}</Div> : children}
+    // </div>
+    <div id={ids[index]} className={classNames[index]}>
+      {index < classNames.length - 1 || index < ids.length - 1 ? (
+        <Div classNames={classNames} ids={ids} index={index + 1}>
+          {children}
+        </Div>
+      ) : (
+        children
+      )}
+    </div>
+  );
+}
