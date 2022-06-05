3d326ac3876860274546fba9d92424bc8b4fd14c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-05 16:55:11 +0300

    add initial footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import Div from '../utils/Div';
+
+export default function Footer() {
+  return (
+    <Div classNames={['rn-footer-area rn-section-gap section-separator', 'container', 'row', 'col-lg-12', 'footer-area text-center']}>
+      <Div classNames={['logo']}>
+        <a href='index.html'>
+          <img src='assets/images/logo/logo-vertical.png' alt='logo' />
+        </a>
+      </Div>
+      <p class='description mt--30'>
+        © 2022. All rights reserved by{' '}
+        <a target='_blank' href='https://themeforest.net/user/rainbow-themes/portfolio' rel='noreferrer'>
+          Rainbow-Themes.
+        </a>
+      </p>
+    </Div>
+  );
+}
