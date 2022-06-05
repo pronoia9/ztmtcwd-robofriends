0345fc569195514eaed9ae1501ae378b0e0a2aa7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-05 16:57:01 +0300

    add new structure + Div component + style classes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+/* eslint-disable no-script-url */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import Div from '../utils/Div';
 
-export default function Card ({ id, name = 'Name', username = 'Username', email = 'example@mail.com' }) {
+export default function Card({ id, name = 'Name', username = 'Username', email = 'example@mail.com' }) {
   return (
-    <div className='gradient-spacey tc dib bd3 pa3 ma2 grow bw2'>
-      <div className='card-container'>
-        <div className='card'>
+    <Div classNames={['col-lg-6 col-xl-4 col-md-6 col-12 mt--50 mt_md--30 mt_sm--30 aos-init aos-animate', 'rn-portfolio', 'inner']}>
+      <Div classNames={['thumbnail']}>
+        <a href='javascript:void(0)'>
           <img src={`https://robohash.org/${id}?200x200`} alt={`${username}_photo`} />
-          <div className='info-container'>
-            <p className='name'>{name}</p>
-            <p className='username'>{`@${username}`}</p>
-            <p className='email'>{email}</p>
-          </div>
-        </div>
-      </div>
-    </div>
+        </a>
+      </Div>
+      <Div classNames={['content']}>
+        <Div classNames={['category-info']}>
+          <Div classNames={['category-list']}>
+            <a href='javascript:void(0)'>{`@${username}`}</a>
+          </Div>
+          <Div classNames={['meta']}>
+            <span>
+              <a href='javascript:void(0)'>
+                <i class='feather-heart'></i>
+              </a>
+              {id}
+            </span>
+          </Div>
+        </Div>
+        <h4 class='title'>
+          <a href='javascript:void(0)'>
+            {name} <br /> {email} <i class='feather-arrow-up-right'></i>
+          </a>
+        </h4>
+      </Div>
+    </Div>
   );
-};
+}
