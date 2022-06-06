1d3f15e28c2d21e7b05c6dfb642183ffc78a27c2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 09:22:03 +0300

    import logger + applyMiddleware(logger)

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { createLogger } from 'redux-logger';
+import logger from 'redux-logger';
 // styles
 import './index.scss';
 // components
 import App from './components/App';
 import { searchRobots } from './redux/reducers';
 
-const logger = createLogger;
 const store = createStore(searchRobots, applyMiddleware(logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
