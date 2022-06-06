6d102f37e53864fabd10aab6a1784d6730130b2d
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 09:53:10 +0300

    add requestRobots reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,6 +1,20 @@
+import { REQUEST_ROBOTS_PENDING, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_FAILED } from './constants';
 import { CHANGE_SEARCH_FIELD } from './constants';
 
 const initialState = { searchField: '' };
+
+export const requestRobots = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case REQUEST_ROBOTS_PENDING:
+      return { ...state, isPending: true };
+    case REQUEST_ROBOTS_SUCCESS:
+      return { ...state, isPending: false, robots: action.payload };
+    case REQUEST_ROBOTS_FAILED:
+      return { ...state, isPending: false, robots: [], error: action.payload };
+    default:
+      return state;
+  }
+};
 
 export const searchRobots = (state = initialState, action = {}) => {
   switch (action.type) {
