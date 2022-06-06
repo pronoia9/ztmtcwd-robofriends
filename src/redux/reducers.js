4ffe536b461c9843ff4f56a80f22937f85d9127c
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 10:52:00 +0300

    separate initialStates

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,22 +1,23 @@
 import { REQUEST_ROBOTS_PENDING, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_FAILED } from './constants';
 import { CHANGE_SEARCH_FIELD } from './constants';
 
-const initialState = { searchField: '' };
+const initialStateRobots = { isPending: false, robots: [], error: '' };
+const initialStateSearch = { searchField: '' };
 
-export const requestRobots = (state = initialState, action = {}) => {
+export const requestRobots = (state = initialStateRobots, action = {}) => {
   switch (action.type) {
     case REQUEST_ROBOTS_PENDING:
       return { ...state, isPending: true };
     case REQUEST_ROBOTS_SUCCESS:
       return { ...state, isPending: false, robots: action.payload };
     case REQUEST_ROBOTS_FAILED:
-      return { ...state, isPending: false, robots: [], error: action.payload };
+      return { ...state, isPending: false, error: action.payload };
     default:
       return state;
   }
 };
 
-export const searchRobots = (state = initialState, action = {}) => {
+export const searchRobots = (state = initialStateSearch, action = {}) => {
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return { ...state, searchField: action.payload };
