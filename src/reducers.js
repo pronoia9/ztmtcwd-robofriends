a8ff6865f2e9a988c013684bb1aa9eb52f9e4ef2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 09:08:08 +0300

    CHANGE_SEARCHFIELD -> CHANGE_SEARCH_FIELD

--- src/reducers.js
+++ src/reducers.js
@@ -1,10 +1,10 @@
-import { CHANGE_SEARCHFIELD } from './constants';
+import { CHANGE_SEARCH_FIELD } from './constants';
 
 const initialState = { searchField: '' };
 
 export const searchRobots = (state = initialState, action = {}) => {
   switch (action.type) {
-    case CHANGE_SEARCHFIELD:
+    case CHANGE_SEARCH_FIELD:
       return { ...state, searchField: action.payload };
     default:
       return state;
