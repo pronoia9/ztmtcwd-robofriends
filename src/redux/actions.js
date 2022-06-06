ac5c6e8f06dbad2a5196c571f8a0aed5641e666e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 09:48:23 +0300

    requestRobots = (dispatch) => { ... }

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,12 @@
-import { CHANGE_SEARCH_FIELD } from "./constants";
+import { REQUEST_ROBOTS_PENDING, REQUEST_ROBOTS_SUCCESS, REQUEST_ROBOTS_FAILED } from './constants';
+import { CHANGE_SEARCH_FIELD } from './constants';
+
+export const requestRobots = (dispatch) => {
+  dispatch({ type: REQUEST_ROBOTS_PENDING });
+  fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((data) => dispatch({ type: REQUEST_ROBOTS_SUCCESS, payload: data }))
+    .catch((error) => dispatch({ type: REQUEST_ROBOTS_FAILED }));
+};
 
 export const setSearchField = (input) => ({ type: CHANGE_SEARCH_FIELD, payload: input });
