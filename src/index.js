84f26729f6f66cace2b5649b306b2447ff99c1e1
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 09:41:03 +0300

    applyMiddleware(thunkMiddleware, logger)

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 // styles
 import './index.scss';
 // components
 import App from './components/App';
 import { searchRobots } from './redux/reducers';
 
-const store = createStore(searchRobots, applyMiddleware(logger));
+const store = createStore(searchRobots, applyMiddleware(thunkMiddleware, logger));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
