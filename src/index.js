32de3a6f4c29e1875ed3135f36eeebda5373f9e0
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 09:08:48 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 // styles
 import './index.scss';
 // components
 import App from './components/App';
+import { searchRobots } from './reducers';
+
+const store = createStore(searchRobots);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
