01887ca271a643ee237938e9aea92521586789d7
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2022-06-06 09:08:37 +0300

    remove (unused) searchField from props

--- src/components/Card/CardList.jsx
+++ src/components/Card/CardList.jsx
@@ -1,7 +1,7 @@
 import Div from '../../utils/Div';
 import Card from './Card';
 
-export default function Cards({ robots, searchKey }) {
+export default function Cards({ robots }) {
   return (
     <Div classNames={['row row--25 mt--10 mt_md--10 mt_sm--10']}>
       {robots.map((robot) => (
